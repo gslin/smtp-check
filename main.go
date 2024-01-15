992fb76616c065b1aaa0fdae00175e8b2ff59bc2
Gea-Suan Lin <gslin@gslin.org>
2024-01-16 01:44:28 +0800

    Support auto-discovery of SMTP servers from MX records.

--- main.go
+++ main.go
@@ -3,46 +3,62 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net"
 	"os"
+	"strings"
 
 	"net/smtp"
 )
 
 func main() {
-	var smtp_from string
-	var smtp_hello string
-	var smtp_port string
-	var smtp_server string
+	var arg_smtp_from string
+	var arg_smtp_hello string
+	var arg_smtp_port string
+	var arg_smtp_server string
 
-	flag.StringVar(&smtp_from, "f", "user@example.com", "SMTP user (used in MAIL FROM)")
-	flag.StringVar(&smtp_hello, "h", "example.com", "SMTP hello (used in HELO/EHLO)")
-	flag.StringVar(&smtp_port, "p", "smtp", "SMTP port")
-	flag.StringVar(&smtp_server, "s", "smtp.google.com", "SMTP hostname")
+	flag.StringVar(&arg_smtp_from, "f", "user@example.com", "SMTP user (used in MAIL FROM)")
+	flag.StringVar(&arg_smtp_hello, "h", "example.com", "SMTP hello (used in HELO/EHLO)")
+	flag.StringVar(&arg_smtp_port, "p", "smtp", "SMTP port")
+	flag.StringVar(&arg_smtp_server, "s", "", "SMTP hostname")
 	flag.Parse()
 
-	rcpts := flag.Args()
-	if (len(rcpts) <= 0) {
+	arg_rcpts := flag.Args()
+	if (len(arg_rcpts) <= 0) {
 		fmt.Printf("Usage:\n  %s [arguments] recipients...\n\nArguments:\n", os.Args[0])
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	fmt.Println("Dialing to " + smtp_server)
-	client, err := smtp.Dial(smtp_server + ":" + smtp_port)
+	for _, arg_rcpt := range arg_rcpts {
+		var smtp_server string
 
-	fmt.Println("Hello: " + smtp_hello)
-	err = client.Hello(smtp_hello)
-	fmt.Println("err = ", err)
+		if arg_smtp_server == "" {
+			rcpt_domain := strings.Split(arg_rcpt, "@")[1]
+			mxs, err := net.LookupMX(rcpt_domain)
+			if err == nil {
+				smtp_server = mxs[0].Host
+			} else {
+				smtp_server = rcpt_domain
+			}
+		} else {
+			smtp_server = arg_smtp_server
+		}
 
-	fmt.Println("MAIL FROM: " + smtp_from)
-	err = client.Mail(smtp_from)
-	fmt.Println("err = ", err)
+		fmt.Printf("Dialing to [%s]\n", smtp_server)
+		client, err := smtp.Dial(smtp_server + ":" + arg_smtp_port)
 
-	for _, rcpt := range rcpts {
-		fmt.Println("RCPT TO - " + rcpt)
-		err = client.Rcpt(rcpt)
-		fmt.Println("err = ", err)
+		fmt.Printf("HELO/EHLO [%s]\n", arg_smtp_hello)
+		err = client.Hello(arg_smtp_hello)
+		fmt.Printf("err = [%s]\n", err)
+
+		fmt.Printf("MAIL FROM: [%s]", arg_smtp_from)
+		err = client.Mail(arg_smtp_from)
+		fmt.Printf("err = [%s]\n", err)
+
+		fmt.Printf("RCPT TO: [%s]\n", arg_rcpt)
+		err = client.Rcpt(arg_rcpt)
+		fmt.Printf("err = [%s]\n", err)
+
+		client.Close()
 	}
-
-	client.Close()
 }
